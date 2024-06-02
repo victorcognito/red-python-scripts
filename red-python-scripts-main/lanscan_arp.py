d9120af7f5e185145e80bcb691fcac1b3ea7d9f4
victorcognito <86253280+victorcognito@users.noreply.github.com>
2024-06-02 11:11:40 +0100

    Update lanscan_arp.py

--- red-python-scripts-main/lanscan_arp.py
+++ red-python-scripts-main/lanscan_arp.py
@@ -4,19 +4,6 @@ import scapy.all as scapy
 # We need to create regular expressions to ensure that the input is correctly formatted.
 import re
 
-# Basic user interface header
-print(r"""______            _     _  ______                 _           _ 
-|  _  \          (_)   | | | ___ \               | |         | |
-| | | |__ ___   ___  __| | | |_/ / ___  _ __ ___ | |__   __ _| |
-| | | / _` \ \ / / |/ _` | | ___ \/ _ \| '_ ` _ \| '_ \ / _` | |
-| |/ / (_| |\ V /| | (_| | | |_/ / (_) | | | | | | |_) | (_| | |
-|___/ \__,_| \_/ |_|\__,_| \____/ \___/|_| |_| |_|_.__/ \__,_|_|""")
-print("\n****************************************************************")
-print("\n* Copyright of David Bombal, 2021                              *")
-print("\n* https://www.davidbombal.com                                  *")
-print("\n* https://www.youtube.com/davidbombal                          *")
-print("\n****************************************************************")
-
 # Regular Expression Pattern to recognise IPv4 addresses.
 ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
 
