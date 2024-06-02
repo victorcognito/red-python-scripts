9cf59f3272f7c11969ad058a72c6b30a1a5255cb
victorcognito <86253280+victorcognito@users.noreply.github.com>
2024-06-02 11:10:39 +0100

    Update change-windows10-mac-address.py

--- red-python-scripts-main/change-windows10-mac-address.py
+++ red-python-scripts-main/change-windows10-mac-address.py
@@ -1,15 +1,4 @@
 #!/usr/bin/env python3
-##################################################################################################
-#Copyright of David Bombal, 2021                                                                 #
-#https://www.davidbombal.com                                                                     #
-#https://www.youtube.com/davidbombal                                                             #
-#                                                                                                #
-# Please note that this code can be improved by using functions. It is not programmed to cater   #
-# for all situations, but to be used as a learning tool.                                         #
-#                                                                                                #
-# This code is provided for educational purposes only. Do good. Be Ethical.                      #
-#                                                                                                #
-##################################################################################################
 
 import subprocess
 import winreg
